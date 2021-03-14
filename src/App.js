125d686a68b8be71ccf1f43f167a6d1bf0ea78eb
TomCS3 <62783156+TomCS3@users.noreply.github.com>
2021-03-14 14:04:39 +0000

    Button, Tasks component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import Header from './components/Header'
+import Tasks from './components/Tasks'
+import { useState } from 'react'
 
 const App = () => {
+  const [tasks, setTasks] = useState ([
+    {
+        id: 1,
+        text: 'Grocery Shopping',
+        day: 'March 15th at 9:00am',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Dentist Appointment',
+        day: 'March 15th at 1:00pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Swimming Club',
+        day: 'March 17th at 7:00pm',
+        reminder: false,
+    },
+  ])
+
   return (
     <div className="container">
       <Header />
+      <Tasks tasks={tasks}/>
     </div>
   )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
 import PropTypes from 'prop-types'
-
+import Button from './Button'
 
 export const Header = ({title}) => {
+   const onClick = () => {
+       console.log('Click')
+   }
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <button className='btn'>Add</button>
+            <Button color='blue' text='Add' onClick=
+            {onClick} />
         </header>
     )
 }
