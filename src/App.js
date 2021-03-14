9e9d6d3644e9b49a0aac95465cc2671306d270fe
TomCS3 <62783156+TomCS3@users.noreply.github.com>
2021-03-14 16:00:54 +0000

    Add Task

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import { useState } from 'react'
+
 import Header from './components/Header'
 import Tasks from './components/Tasks'
-import { useState } from 'react'
+import AddTask from './components/AddTask'
 
 const App = () => {
   const [tasks, setTasks] = useState ([
@@ -40,6 +42,7 @@ const App = () => {
   return (
     <div className="container">
       <Header />
+      <AddTask />
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} 
         onDelete={deleteTask} 
