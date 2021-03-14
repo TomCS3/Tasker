035036920137d2df44f74298b6127994e6bfe938
TomCS3 <62783156+TomCS3@users.noreply.github.com>
2021-03-14 12:34:55 +0000

    App Intialised

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types'
+
+
+export const Header = ({title}) => {
+    return (
+        <header className='header'>
+            <h1>{title}</h1>
+            <button className='btn'>Add</button>
+        </header>
+    )
+}
+
+Header.defaultProps = {
+    title: 'Tasker',
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+export default Header
